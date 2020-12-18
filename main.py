024935a4b6e16443790fedfe6aaba34190f7660e
Devang Kamble <risingentropy20@gmail.com>
2020-12-18 21:36:14 +0530

    First Print

---
+++ main.py
@@ -0,0 +1,54 @@
+# sheet size is 2480 * 3508
+# we have margins of 40 and 14
+
+from PIL import Image
+import os
+
+img = Image.open("Canvas/sheet.png")
+img.save("sheet.png")
+
+print("Choose Your Font:")
+print("1. Devang")
+print("Your Choice: ", end='')
+choice = int(input())
+if choice == 1:
+
+    #open the file
+    f = open('data.txt', 'r')
+    data = f.read()
+    xP = 40
+    yP = 14
+    for i in data:
+        try:
+            #open the image
+            image = Image.open('sheet.png')
+            #os.remove('sheet.png')
+            if i == " ":
+                char = "DevangFont/space.png"
+            elif i == ".":
+                char = "DevangFont/dot.png"
+            elif i == '\n':
+                xP = 40
+                yP += 120
+                if yP > 3494:
+                    break
+                continue
+            else:
+                char = i+".png"
+                char = "DevangFont/" + char
+            img2 = Image.open(char)
+            pos = ((xP), (yP))
+            image.paste(img2, pos, img2)
+            image.save("sheet.png")
+            xP += 80
+            if xP > 2440:
+                xP = 40
+                yP += 120
+                if yP > 3494:
+                    break
+
+        except:
+            print("Invalid Character")
+    print("Image is now Complete.")
+else:
+    print("Invalid Choice.")
