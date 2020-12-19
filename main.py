1168c8a159ac42d57c28a0d9916ca0b3605e9993
Devang Kamble <risingentropy20@gmail.com>
2020-12-19 09:31:02 +0530

    - edge case

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # sheet size is 2480 * 3508
-# we have margins of 40 and 14
+# we have margins of 120 and 48
 
 from PIL import Image
 import os
@@ -8,7 +8,7 @@ img = Image.open("Canvas/sheet.png")
 img.save("sheet.png")
 
 print("Choose Your Font:")
-print("1. Devang")
+print("1. Lamptey")
 print("Your Choice: ", end='')
 choice = int(input())
 if choice == 1:
@@ -16,43 +16,59 @@ if choice == 1:
     #open the file
     f = open('data.txt', 'r')
     data = f.read()
-    xP = 40
-    yP = 14
-    for i in data:
+    xP = 120
+    yP = 48
+    data += " "
+    for i in range(len(data)):
         try:
+            if i == len(data) - 1:
+                continue
             #open the image
             image = Image.open('sheet.png')
             #os.remove('sheet.png')
-            if i == " ":
-                char = "DevangFont/space.png"
-            elif i == ".":
-                char = "DevangFont/dot.png"
-            elif i == "~":
-                char = "DevangFont/tilde.png"
-            elif i == "/":
-                char = "DevangFont/fslash.png"
-            elif i == '\n':
-                xP = 40
+            if data[i] == " ":
+                char = "LampteyFont/space.png"
+            elif data[i] == ".":
+                char = "LampteyFont/dot.png"
+            elif data[i] == "~":
+                char = "LampteyFont/tilde.png"
+            elif data[i] == "/":
+                char = "LampteyFont/fslash.png"
+            elif data[i] == '\n':
+                xP = 120
                 yP += 120
                 if yP > 3494:
                     break
                 continue
             else:
-                char = i+".png"
-                char = "DevangFont/" + char
+                char = data[i]+".png"
+                char = "LampteyFont/" + char
             img2 = Image.open(char)
             pos = ((xP), (yP))
             image.paste(img2, pos, img2)
             image.save("sheet.png")
             xP += 60
-            if xP > 2440:
-                xP = 40
+            if xP > 2360:
+                xP = 120
                 yP += 120
-                if yP > 3494:
+                if yP > 3460:
                     break
-
+            if xP >= 2240:
+                if data[i+1] == " " or data[i+1] == "\n":
+                    continue
+                else:
+                    char = "LampteyFont/-.png"
+                    img2 = Image.open(char)
+                    pos = ((xP), (yP))
+                    image.paste(img2, pos, img2)
+                    image.save("sheet.png")
+                    xP = 120
+                    yP += 120
+                    if yP > 3460:
+                        break
         except:
             print("Invalid Character")
+            break
     print("Image is now Complete.")
 else:
     print("Invalid Choice.")
