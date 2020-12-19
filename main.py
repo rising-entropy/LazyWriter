f6f4eacc46a0a0e5f4bbc77a100e67b60d082ba8
Devang Kamble <risingentropy20@gmail.com>
2020-12-19 17:27:50 +0530

    Mersey

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ img = Image.open("Canvas/sheet.png")
 img.save("sheet.png")
 
 print("Choose Your Font:")
-print("1. Lamptey")
+print("1. Lamptey\n2. Alexandria")
 print("Your Choice: ", end='')
 choice = int(input())
 if choice == 1:
@@ -70,5 +70,67 @@ if choice == 1:
             print("Invalid Character")
             break
     print("Image is now Complete.")
+
+elif choice == 2:
+    #open the file
+    f = open('data.txt', 'r')
+    data = f.read()
+    xP = 120
+    yP = 48
+    data += " "
+    for i in range(len(data)):
+        try:
+            if i == len(data) - 1:
+                continue
+            #open the image
+            image = Image.open('sheet.png')
+            #os.remove('sheet.png')
+            if data[i] == " ":
+                char = "AlexandriaFont/space.png"
+            elif data[i] == ".":
+                char = "AlexandriaFont/dot.png"
+            elif data[i] == "~":
+                char = "AlexandriaFont/tilde.png"
+            elif data[i] == "/":
+                char = "AlexandriaFont/fslash.png"
+            elif data[i] == '\n':
+                xP = 120
+                yP += 120
+                if yP > 3494:
+                    break
+                continue
+            else:
+                char = data[i]+".png"
+                char = "AlexandriaFont/" + char
+            img2 = Image.open(char)
+            (x, y) = img2.size
+            pos = ((xP), (yP))
+            image.paste(img2, pos, img2)
+            image.save("sheet.png")
+            xP += x
+            if xP > 2360:
+                xP = 120
+                yP += 120
+                if yP > 3460:
+                    break
+            if xP >= 2240:
+                if data[i+1] == " " or data[i+1] == "\n" or data[i+1] == "'" or data[i+1] == "-":
+                    continue
+                else:
+                    char = "AlexandriaFont/-.png"
+                    img2 = Image.open(char)
+                    pos = ((xP), (yP))
+                    image.paste(img2, pos, img2)
+                    image.save("sheet.png")
+                    xP = 120
+                    yP += 120
+                    if yP > 3460:
+                        break
+        except:
+            print("Invalid Character")
+            break
+    print("Image is now Complete.")
+
+
 else:
     print("Invalid Choice.")
